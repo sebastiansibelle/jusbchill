837fa11ee32b36c31d5f518fba4cdf9f2c82068e
Sebastian Sibelle <sebastian.sibelle@squareweave.com.au>
2014-08-21 18:58:49 +1000

    Added front end for releases

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @artists = Artist.all
+    @releases = Release.all
   end
 end
--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -3,4 +3,8 @@ class ReleasesController < ApplicationController
   def index
     @releases = Release.all
   end
+
+  def show
+    @release = Release.friendly.find(params[:id])
+  end
 end
