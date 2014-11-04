4fc759f0f7b6012f805e534a1f8dba716e9cfd49
Sebastian Sibelle <sebastiansibelle@gmail.com>
2014-11-04 13:24:44 +1100

    Added postgresql

--- Gemfile
+++ Gemfile
@@ -5,8 +5,9 @@ gem 'rails', '4.1.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
-# Use postgres as the database for Active Record
-#gem 'pg'
+# Use postgres as the database for Active 
+gem 'pg'
+gem 'taps'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user1 = User.create! :email => "sebastian@jusbchill.com", :password => "admin", :password_confirmation => 'admin'
-user2 = User.create! :email => "andrewjosephgoddard@gmail.com", :password => "admin", :password_confirmation => 'admin'
-user3 = User.create! :email => "aaron@jusbchill.com", :password => "admin", :password_confirmation => 'admin'
-user4 = User.create! :email => "jacob@jusbchill.com", :password => "admin", :password_confirmation => 'admin'
+user1 = User.create! :email => "sebastian@jusbchill.com", :password => "jusbadmin", :password_confirmation => 'jusbadmin'
+user2 = User.create! :email => "andrewjosephgoddard@gmail.com", :password => "jusbadmin", :password_confirmation => 'jusbadmin'
+user3 = User.create! :email => "aaron@jusbchill.com", :password => "jusbadmin", :password_confirmation => 'jusbadmin'
+user4 = User.create! :email => "jacob@jusbchill.com", :password => "jusbadmin", :password_confirmation => 'jusbadmin'
