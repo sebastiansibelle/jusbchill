7ecd7b0dcf26f093ed99bbb2ca4a50fa3da2244f
Sebastian Sibelle <sebastiansibelle@gmail.com>
2014-11-08 17:58:47 +1100

    Only compile admin and application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile = ['application.js', 'application.css', 'admin.css']
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
