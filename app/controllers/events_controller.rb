f7d2d8d925ff5cdd1da903fab4ecc81fde86e30c
Sebastian Sibelle <sebastiansibelle@gmail.com>
2015-02-24 00:15:30 +1100

    Added cool room event page

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,22 @@
+class EventsController < ApplicationController
+  # GET /events
+  # GET /events.json
+  def index
+    @events = Event.all
+  end
+
+  # GET /events/1
+  # GET /events/1.json
+  def show
+    @event = Event.friendly.find(params[:id])
+
+    if @event.published == false
+      if params[:secret] != @event.secret_hash
+        raise ActionController::RoutingError.new('Not Found')
+      end
+    end
+  end
+
+  def coolroom
+  end
+end
