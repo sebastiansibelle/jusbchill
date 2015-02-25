1ec189dd87dd8837c53b5626e686b93f1ba5746a
Sebastian Sibelle <sebastiansibelle@gmail.com>
2015-02-25 19:57:20 +1100

    Added the pilot url as a valid url

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   get 'privacy' => 'home#privacy'
   get 'legal' => 'home#legal'
   get 'about' => 'home#about'
+  get 'coolroom/pilot' => 'events#coolroom'
   get 'coolroom' => 'events#coolroom'
 
   # You can have the root of your site routed with "root"
