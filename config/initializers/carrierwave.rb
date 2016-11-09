abcb45828789af76a0af16878faf2417c8a707d3
Sebastian Sibelle <sebastiansibelle@gmail.com>
2016-11-09 14:29:18 +1100

    Added an intialiser for carrierwave uploads

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+    region:                'ap-southeast-2',                  # optional, defaults to 'us-east-1'
+    host:                  's3-ap-southeast-2.amazonaws.com',             # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'jusbchill-production/uploads'                          # required
+  config.fog_public     = true                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
